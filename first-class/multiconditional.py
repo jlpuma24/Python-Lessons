13265be0483e14eba5ec44f73a4e557b21756162
Natalia Cardona <natathemilk@gmail.com>
2026-04-21 19:00:56 -0400

    unblock second lesson-update limit_weigth_men

--- first-class/multiconditional.py
+++ first-class/multiconditional.py
@@ -1,7 +1,12 @@
 weigth = 90
 is_women = False
 limit_weigth = 80
+limit_weigth_men = 85
 if is_women == True and weigth <= limit_weigth:
     print('is healthly')
 elif is_women == True and weigth > limit_weigth:
+    print('is unhealthly')
+elif is_women == False and weigth <= limit_weigth_men:
+    print('is healthly')
+elif is_women == False and weigth > limit_weigth_men:
     print('is unhealthly')
